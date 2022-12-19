99d0bff8e7d3e07e752f4af963c9f7f26534ba94
sutrisno <trisj18@gmail.com>
2022-12-19 21:49:40 +0700

    add migration table

---
+++ database/migrations/2022_12_19_133435_create_detail_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_user', function (Blueprint $table) {
+            $table->id();
+            $table-integer('user_id')->nullable();
+            $table-integer('type_user_id')->nullable();
+            $table-string('contact')->nullable();
+            $table-longText('address')->nullable();
+            $table-longText('photo')->nullable();
+            $table-enum('gender',[1,2])->nullable();
+            $table->timestamps();
+            $table->softdeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_user');
+    }
+}
